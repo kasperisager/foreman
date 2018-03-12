2e97a6da6eab102f59040664d6edcab6dcaef74e
Kasper Isager <kasperisager@gmail.com>
2018-03-12 13:02:45 +0100

    Kill off TypeDoc package for now

--- packages/typedoc/test/reflect.spec.ts
+++
@@ -1,7 +0,0 @@
-import { reflect } from "../src/reflect";
-
-const { test } = require("tap");
-
-test("reflect", async (t: any) => {
-  console.log(await reflect(require.resolve("./fixtures/test.ts")));
-});
