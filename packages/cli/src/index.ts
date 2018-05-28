e1b090972229090c9e9b41f59c72dbd1a2cd3681
Kasper Isager <kasperisager@gmail.com>
2018-05-28 17:13:32 +0200

    Enable generation of profiler output

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,14 +4,23 @@ const exec = require("execa");
 
 const { argv } = yargs.command("* <script>", "", {
   builder: (argv: any) =>
-    argv.positional("script", {
-      description: "The script to execute"
-    }),
+    argv
+      .option("prof", {
+        type: "boolean",
+        description: "Generate profiler output"
+      })
+      .positional("script", {
+        description: "Script to evaluate"
+      }),
   handler: (argv: any) => {
     const script = path.resolve(argv.script);
 
     if (script !== null) {
       const args = ["--require", require.resolve("@foreman/register")];
+
+      if (argv.prof) {
+        args.push("--prof");
+      }
 
       args.push(script);
 
