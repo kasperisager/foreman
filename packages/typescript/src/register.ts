3d85a6cc3a7b6661a8a760187c37ea3da30e5eac
Kasper Isager <kasperisager@gmail.com>
2018-06-01 14:02:52 +0200

    Do type-guided emits in register hook

--- packages/typescript/src/register.ts
+++ packages/typescript/src/register.ts
@@ -1,9 +1,30 @@
 import { register } from "@foreman/api";
+import { extension } from "@foreman/path";
+import { Workspace } from "typecomp";
 import { transform } from "./transform";
+
+const workspace = new Workspace();
 
 register({
   extensions: [".ts", ".tsx"],
   transform: (source, filename) => {
-    return transform(source, { fileName: filename });
+    const output = workspace.compile(filename);
+
+    let code: string | undefined;
+    let map: string | undefined;
+
+    for (const { name, text } of output) {
+      switch (extension(name)) {
+        case ".js":
+          code = text;
+          break;
+        case ".map":
+          map = text;
+      }
+    }
+
+    return code === undefined
+      ? transform(source, { fileName: filename })
+      : { code, map };
   }
 });
